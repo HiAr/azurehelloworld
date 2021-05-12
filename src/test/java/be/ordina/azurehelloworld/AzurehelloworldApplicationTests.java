9a2793dcb32d6bc910a842e5d9ac2fc8cc3c8b66
Dries Thieren <dries.thieren@gmail.com>
2021-05-12 17:36:47 +0200

    first commit

---
+++ src/test/java/be/ordina/azurehelloworld/AzurehelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ordina.azurehelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurehelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
